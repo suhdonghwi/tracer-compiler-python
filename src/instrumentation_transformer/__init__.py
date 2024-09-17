1d111d6a4b49ba1095773127c88074cc1c2a4a09
Suh Donghwi <hwidongsuh@gmail.com>
2024-09-18 00:35:15 +0900

    location maps

--- src/instrumentation_transformer/__init__.py
+++ src/instrumentation_transformer/__init__.py
@@ -1,7 +1,6 @@
 import ast
 import typing
 
-from id_mapped_source_file import IdMappedSourceFile
 from instrumentation_transformer.ast_utils import (
     is_invoking_expr,
     is_invoking_stmt,
@@ -10,17 +9,18 @@ from instrumentation_transformer.ast_utils import (
     wrap_with_expr_begin_end,
     wrap_with_frame_begin_end,
 )
+from node_id_mapped_ast import NodeIdMappedAST
 
 
 class InstrumentationTransformer(ast.NodeTransformer):
-    def __init__(self, id_mapped_source_file: IdMappedSourceFile):
-        self.id_mapped_source_file = id_mapped_source_file
+    def __init__(self, node_id_mapped_ast: NodeIdMappedAST):
+        self.node_id_mapped_ast = node_id_mapped_ast
 
     def transform(self):
-        return ast.fix_missing_locations(self.visit(self.id_mapped_source_file.ast))
+        return ast.fix_missing_locations(self.visit(self.node_id_mapped_ast.raw_ast))
 
     def visit(self, node: ast.AST) -> ast.AST:
-        node_id = self.id_mapped_source_file.get_node_id(node)
+        node_id = self.node_id_mapped_ast.get_node_id(node)
         node_id_node = make_uuid_node(node_id)
 
         if isinstance(node, ast.FunctionDef):
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import ast
+import json
 import os
 import sys
+from typing import Tuple
 
-from id_mapped_source_file import IdMappedSourceFile
 from instrumentation_transformer import InstrumentationTransformer
+from node_id_mapped_ast import NodeId, NodeIdMappedAST
 from source_file import SourceFile
 
 
@@ -19,6 +21,29 @@ def get_py_files_inside_directory(directory_path: str) -> list[str]:
     return paths
 
 
+def make_location_map(source_file: SourceFile, node_id_mapped_ast: NodeIdMappedAST):
+    node_id_to_range: dict[NodeId, Tuple[int, int]] = {}
+
+    for node_id, node in node_id_mapped_ast.items():
+        if isinstance(node, ast.Module):
+            node_id_to_range[node_id] = (0, len(source_file.content))
+        elif isinstance(node, (ast.stmt, ast.expr)):
+            if node.end_col_offset is None:
+                raise ValueError("Node has no end_col_offset")
+
+            node_id_to_range[node_id] = (node.col_offset, node.end_col_offset)
+
+    node_id_to_range_json_dict = {
+        str(node_id): (start, end) for node_id, (start, end) in node_id_to_range.items()
+    }
+
+    return json.dumps({
+        "path": source_file.path,
+        "content": source_file.content,
+        "node_id_to_range": node_id_to_range_json_dict,
+    })
+
+
 if __name__ == "__main__":
     input_path = sys.argv[1]
     source_file_paths = []
@@ -29,8 +54,7 @@ if __name__ == "__main__":
         source_file_paths = [input_path]
 
     source_files = [
-        SourceFile.from_path(source_file_path)
-        for source_file_path in source_file_paths
+        SourceFile.from_path(source_file_path) for source_file_path in source_file_paths
     ]
 
     if len(source_files) == 0:
@@ -38,12 +62,15 @@ if __name__ == "__main__":
         sys.exit(1)
 
     print(len(source_files), "source files found")
-    id_mapped_source_files = [
-        IdMappedSourceFile(source_file) for source_file in source_files
-    ]
+    location_maps: list[str] = []
 
-    for id_mapped_source_file in id_mapped_source_files:
-        transformed_ast = InstrumentationTransformer(id_mapped_source_file).transform()
-        print(id_mapped_source_file.dumps())
-        # print(ast.unparse(transformed_ast))
+    for source_file in source_files:
+        raw_ast = ast.parse(source_file.content)
+        node_id_mapped_ast = NodeIdMappedAST(raw_ast)
+        transformed_ast = InstrumentationTransformer(node_id_mapped_ast).transform()
 
+        location_map = make_location_map(source_file, node_id_mapped_ast)
+        location_maps.append(location_map)
+
+    print(location_maps)
+        # print(source_file.path)
