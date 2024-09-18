47fffc6ed8868c100a719e5dc07c2d55f261799b
Suh Donghwi <hwidongsuh@gmail.com>
2024-09-18 19:37:57 +0900

    file processing util file

--- src/main.py
+++ src/main.py
@@ -1,18 +1,13 @@
 import ast
 import os
-import shutil
 import sys
 from pathlib import Path
-from typing import Tuple
 
+from file_processing import clear_directory, get_source_dest_pairs, write_to_path
 from instrumentation_transformer import InstrumentationTransformer
 from location_map import make_location_map
 from node_id_mapped_ast import NodeIdMappedAST
 from source_file import SourceFile
-
-
-def get_py_files_inside_directory(directory_path: Path) -> list[Path]:
-    return [file_path for file_path in directory_path.rglob("*.py")]
 
 
 def construct_import_from_node(
@@ -45,53 +40,27 @@ def process_source_file(source_file: SourceFile):
     return instrumented_code, location_map
 
 
-def write_instrumented_code(output_path: Path, instrumented_code: str):
-    output_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(output_path, "w", encoding="utf-8") as output_file:
-        output_file.write(instrumented_code)
-
-
-def clear_output_directory(output_directory: Path):
-    if output_directory.exists() and output_directory.is_dir():
-        shutil.rmtree(output_directory)
-    output_directory.mkdir(parents=True, exist_ok=True)
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: script.py <input_path> <output_directory>")
         sys.exit(1)
 
     input_path = Path(sys.argv[1])
-    output_path = Path(sys.argv[2])
+    output_directory_path = Path(sys.argv[2])
 
-    if not output_path.exists():
-        output_path.mkdir(parents=True, exist_ok=True)
+    if not output_directory_path.exists():
+        output_directory_path.mkdir(parents=True, exist_ok=True)
 
-    source_dest_pairs: list[Tuple[Path, Path]] = []
-
-    if input_path.is_file():
-        source_dest_pairs = [(input_path, output_path / input_path.name)]
-    elif input_path.is_dir():
-        source_dest_pairs = [
-            (
-                py_file_path,
-                output_path / py_file_path.relative_to(input_path),
-            )
-            for py_file_path in get_py_files_inside_directory(input_path)
-        ]
+    source_dest_pairs = get_source_dest_pairs(input_path, output_directory_path)
 
     if len(source_dest_pairs) == 0:
         print("No source files found")
         sys.exit(1)
 
-    clear_output_directory(output_path)
+    clear_directory(output_directory_path)
 
-    for source_file_path, output_path in source_dest_pairs:
+    for source_file_path, destination_path in source_dest_pairs:
         source_file = SourceFile.from_path(source_file_path)
-
         instrumented_code, location_map = process_source_file(source_file)
-        if instrumented_code:
-            write_instrumented_code(output_path, instrumented_code)
-        else:
-            print(f"Failed to process file: {source_file.path}")
+
+        write_to_path(destination_path, instrumented_code)
