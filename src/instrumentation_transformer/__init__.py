7bd0130596d166a8c3b833e36a91637d3560cc1b
Suh Donghwi <hwidongsuh@gmail.com>
2024-09-18 20:57:29 +0900

    make use of typing.overload

--- src/instrumentation_transformer/__init__.py
+++ src/instrumentation_transformer/__init__.py
@@ -1,5 +1,5 @@
 import ast
-import typing
+from typing import overload
 
 from instrumentation_transformer.ast_utils import (
     is_invoking_expr,
@@ -17,35 +17,38 @@ class InstrumentationTransformer(ast.NodeTransformer):
         self.node_id_mapped_ast = node_id_mapped_ast
 
     def transform(self):
-        return ast.fix_missing_locations(
-            typing.cast(ast.Module, self.visit(self.node_id_mapped_ast.raw_ast))
-        )
+        return ast.fix_missing_locations(self.visit(self.node_id_mapped_ast.raw_ast))
 
-    def visit_expr(self, node: ast.expr) -> ast.expr:
-        return typing.cast(ast.expr, self.visit(node))
+    @overload
+    def visit(self, node: ast.expr) -> ast.expr:
+        ...
 
-    def visit_stmt(self, node: ast.stmt) -> ast.stmt:
-        return typing.cast(ast.stmt, self.visit(node))
+    @overload
+    def visit(self, node: ast.stmt) -> ast.stmt:
+        ...
+
+    @overload
+    def visit(self, node: ast.Module) -> ast.Module:
+        ...
+
+    @overload
+    def visit(self, node: ast.AST) -> ast.AST:
+        ...
 
     def visit(self, node: ast.AST) -> ast.AST:
         node_id = self.node_id_mapped_ast.get_node_id(node)
         node_id_node = make_uuid_node(node_id)
 
         if isinstance(node, ast.FunctionDef):
-            node.body = list(map(self.visit_stmt, node.body))
-
+            node.body = list(map(self.visit, node.body))
             return node
 
         elif isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load):
             return node
 
-        elif isinstance(node, (ast.Assign, ast.AugAssign)):
-            node.value = self.visit_expr(node.value)
-            return wrap_with_stmt_begin_end(node, node_id_node)
-
-        elif isinstance(node, ast.AnnAssign):
+        elif isinstance(node, (ast.Assign, ast.AugAssign, ast.AnnAssign)):
             if node.value is not None:  # type: ignore
-                node.value = self.visit_expr(node.value)
+                node.value = self.visit(node.value)
 
             return wrap_with_stmt_begin_end(node, node_id_node)
 
