528dbdbabedd2abac20eca2c5fa3f9ee5f3a08f9
Suh Donghwi <hwidongsuh@gmail.com>
2024-09-21 21:09:51 +0900

    some refactors

--- src/instrument/__init__.py
+++ src/instrument/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from .node_id_mapping import NodeIdMapping
 from .ast_transformer import InstrumentationTransformer
 from .tracer_metadata import make_tracer_metadata_json
+from .tracer_module_import import make_tracer_module_import_node
 
 
 def instrument_code(
@@ -22,7 +23,7 @@ def instrument_code(
 
     instrumented_ast.body = [
         make_tracer_module_import_node(
-            destination_path, tracer_module_path, "__tracer__"
+            destination_path, tracer_module_path
         )
     ] + instrumented_ast.body
 
@@ -30,39 +31,3 @@ def instrument_code(
     metadata_json = make_tracer_metadata_json(code, source_path, node_id_mapping)
 
     return instrumented_code, metadata_json
-
-
-def make_tracer_module_import_node(
-    importer_path: Path, import_target_path: Path, identifier: str
-):
-    relative_path = relative_walk_up(import_target_path, importer_path.parent)
-    relative_path_parts = relative_path.parts
-
-    parent_dir_count = sum(1 for part in relative_path_parts if part == "..")
-
-    level = parent_dir_count + 1
-    module_name = ".".join(relative_path_parts[parent_dir_count:]).rsplit(".py", 1)[0]
-
-    return ast.ImportFrom(
-        module=module_name,
-        names=[ast.alias(name=identifier, asname=None)],
-        level=level,
-    )
-
-
-# pathlib relative_to method does not support walk_up option under Python 3.12
-def relative_walk_up(path: Path, base: Path) -> Path:
-    common_path = Path(*base.parts)
-    while common_path not in path.parents and len(common_path.parts) > 0:
-        common_path = common_path.parent
-
-    if common_path == Path("/"):
-        raise ValueError(f"Cannot find a relative path from {path} to {base}")
-
-    # Number of `..` required to reach the common path
-    up_steps = len(base.parts) - len(common_path.parts)
-
-    # Calculate the relative path from the common base
-    relative_path = Path(*[".."] * up_steps) / path.relative_to(common_path)
-
-    return relative_path
--- src/utils/file_processing.py
+++ src/utils/file_processing.py
@@ -1,9 +1,11 @@
 import shutil
 from pathlib import Path
 
+
 def get_files_inside_directory(directory_path: Path) -> list[Path]:
     return [
-        path for path in directory_path.rglob("*") 
+        path
+        for path in directory_path.rglob("*")
         if path.is_file() and "__pycache__" not in path.parts
     ]
 
@@ -18,3 +20,21 @@ def write_to_ensured_path(destination_path: Path, content: str):
     destination_path.parent.mkdir(parents=True, exist_ok=True)
     with open(destination_path, "w", encoding="utf-8") as output_file:
         output_file.write(content)
+
+
+# pathlib relative_to method does not support walk_up option under Python 3.12
+def relative_walk_up(path: Path, base: Path) -> Path:
+    common_path = Path(*base.parts)
+    while common_path not in path.parents and len(common_path.parts) > 0:
+        common_path = common_path.parent
+
+    if common_path == Path("/"):
+        raise ValueError(f"Cannot find a relative path from {path} to {base}")
+
+    # Number of `..` required to reach the common path
+    up_steps = len(base.parts) - len(common_path.parts)
+
+    # Calculate the relative path from the common base
+    relative_path = Path(*[".."] * up_steps) / path.relative_to(common_path)
+
+    return relative_path
