d324b129149fa40660b6027d189c3dd513256f52
Suh Donghwi <hwidongsuh@gmail.com>
2024-10-06 20:49:41 +0900

    use orjson if possible

--- src/source_tracer_module/__init__.py
+++ src/source_tracer_module/__init__.py
@@ -1,11 +1,19 @@
 from pathlib import Path
-from typing import Any
-import json
+from typing import Any, cast
+
+try:
+    import orjson as json
+
+    orjson_available = True
+except ImportError:
+    import json
+    from .json_encoder import DataclassJSONEncoder
+
+    orjson_available = False
 
 
 from .metadata import load_metadata_files
 from .trace_manager import FrameTrace, TraceManager
-from .json_encoder import DataclassJSONEncoder
 
 
 root_path = Path(__file__).parent.parent
@@ -16,10 +24,19 @@ trace = TraceManager()
 
 
 def write_trace_output(frame_trace: FrameTrace):
+    frame_trace_json = cast(
+        str,
+        json.dumps(frame_trace).decode("utf-8")  # type: ignore
+        if orjson_available
+        else json.dumps(frame_trace, cls=DataclassJSONEncoder),  # type: ignore
+    )
+
+    metadata_list_json = "[" + ",".join(metadata_file_strings) + "]"
+
     trace_output_json = (
         "{"
-        '"metadata_list": [' + ",".join(metadata_file_strings) + "],"
-        '"trace": ' + json.dumps(frame_trace, cls=DataclassJSONEncoder) + "}"
+        '"metadata_list": ' + metadata_list_json + ","
+        '"trace": ' + frame_trace_json + "}"
     )
 
     output_file_path = Path(__file__).parent / "trace_output.json"
