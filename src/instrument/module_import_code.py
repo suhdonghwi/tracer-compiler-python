0d518f93311733f47d64f488daf58af22744e45f
Suh Donghwi <hwidongsuh@gmail.com>
2024-11-03 23:33:04 +0900

    change module name

--- src/instrument/module_import_code.py
+++ src/instrument/module_import_code.py
@@ -1,4 +1,4 @@
 def make_runtime_module_import_code():
-    import_code = "\n".join(["import tracer_runtime"])
+    import_code = "\n".join(["import tracer_runtime_python"])
 
     return import_code
